56daaa4dd19fbfa12bf574000d66cf9eaa3348fe
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-27 15:50:48 -0500

    update sizes of about-me img and hero title for smaller screens

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -4,7 +4,7 @@ const AboutMe = () => {
   return (
     <section className="about-me">
       <h1 className="title-section">About me</h1>
-      <div className="profile-img-container h-[21rem] w-[21rem]  overflow-hidden rounded-full md:h-[26rem] md:w-[26rem] lg:h-[26rem] lg:w-[26rem] xl:h-[28rem] xl:w-[28rem] 2xl:h-[32rem] 2xl:w-[32rem]">
+      <div className="profile-img-container h-[17.5rem] w-[17.5rem]  overflow-hidden rounded-full md:h-[24rem] md:w-[24rem] lg:h-[26rem] lg:w-[26rem] xl:h-[28rem] xl:w-[28rem] 2xl:h-[32rem] 2xl:w-[32rem]">
         <img className="profile-img" src={profileImg} alt="Profile image" />
       </div>
       <div className="about-me-information">
