6b917d1fb85fb22ca075d05353587ccf3254e6ed
Alex Martinez <alexmartinez.mm98@gmail.com>
2023-09-26 20:07:09 -0500

    update projects info and add hyperlinks

--- src/components/ProjectDetail/index.jsx
+++ src/components/ProjectDetail/index.jsx
@@ -34,7 +34,25 @@ export const ProjectDetail = ({
       <figure className="flex h-min flex-row   ">
         <div>
           <h4 className="mt-3 text-3xl font-bold tracking-tight md:text-4xl">
-            {name} <span> </span>
+            {name}{" "}
+            <span>
+              {" "}
+              <svg
+                className="hyperlink-icon ml-2.5 inline-flex h-4 w-4 items-center hover:underline lg:h-5 lg:w-5 xl:h-6 xl:w-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+                />
+              </svg>
+            </span>
           </h4>
           <p className="mt-4 text-xl md:text-2xl">{description}</p>
           <div className="mt-4 flex flex-wrap gap-x-16 gap-y-2 text-lg">
