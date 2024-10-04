054fa33dae5c734d85c74e5c67b5eb0e9e564774
Alex Martinez <alexmartinez.mm98@gmail.com>
2024-10-04 17:46:43 -0500

    update margin

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,7 +3,7 @@ import "./index.css"
 const AboutMe = () => {
   return (
     <section className="about-me" id="aboutMe">
-      <h1 className="title-section">About me</h1>
+      <h1 className="title-section lg:mb-10 xl:mb-14">About me</h1>
       <div className="profile-img-container h-[17.5rem] w-[17.5rem]  overflow-hidden rounded-full md:h-[24rem] md:w-[24rem] lg:h-[26rem] lg:w-[26rem] xl:h-[28rem] xl:w-[28rem] 2xl:h-[32rem] 2xl:w-[32rem]">
         <img className="profile-img" src={profileImg} alt="Profile image" />
       </div>
